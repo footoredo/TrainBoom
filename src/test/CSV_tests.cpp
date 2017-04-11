9f54b82e49dcd2a25df458b5f7c2ac7330082344
footoredo <footoredo@gmail.com>
2017-04-11 10:07:03 +0800

    clear up *~ files

--- src/test/CSV_tests.cpp
+++ src/test/CSV_tests.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <cstdio>
-#include "util/CSV.hpp" 
+#include "util/CSV.hpp"
 using namespace std;
 int main(int argc, char *argv[])
 {
     using util::CSV;
 	CSV a,b;
-	b.load("test.csv");
-	a=b; 
+	b.load("test/test.csv");
+	a=b;
 	cout<<a.size()<<' '<<a.size(2)<<endl;
-	cout<<a.data(569,1)<<endl; 
+	cout<<a.data(569,1)<<endl;
 	return 0;
 }
