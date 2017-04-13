15dd5745a304010c6dab93bf9b2ddbb0cefad960
footoredo <footoredo@gmail.com>
2017-04-14 00:15:27 +0800

    Fix test_runner.cpp

--- src/test_runner.cpp
+++ src/test_runner.cpp
@@ -7,5 +7,6 @@ const std::string BIN = "test_bin/";
 int main(int argc, char **argv) {
     std::string object_name(argv[1]);
 
+    system(("make -B test_" + object_name).c_str());
     system(("./" + BIN + object_name + "_tests").c_str());
 }
