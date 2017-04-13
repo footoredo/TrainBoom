decf453155e75a5c4d3722b582241ff380dc281d
footoredo <footoredo@gmail.com>
2017-04-14 00:46:51 +0800

    Update Ticket

--- src/Ticket.hpp
+++ src/Ticket.hpp
@@ -4,45 +4,70 @@
 #include <cstring>
 #include <string>
 #include <iostream>
+#include "exception.hpp"
 using std :: ostream;
 
-class Ticket{
+namespace Ticket {
+
+typedef std::string Type;
+
+class Attribute {
 	private:
-		std::string type; // 车票种类（二等/一等/商务）
+		// std::string type; // 车票种类（二等/一等/商务）
 		double price; // 车票单价
-		// size_t number;  // 车票余量
+		size_t number;  // 车票余量
 
 	public:
-		Ticket(): type(), price() { // 无参构造，类型为空
+		Attribute(): price(), number() { // 无参构造，类型为空
 		}
 
-		Ticket(const std::string s, double p): type(s), price(p) {}
+		Attribute(double p, size_t n): price(p), number(n) {}
 
-		Ticket(const Ticket &t){
-			price = t.price;
+		Attribute(const Attribute &t): price(t.price), number(t.number) {
+			// price = t.price;
 			// number = t.number;
-			type = t.type;
+			// type = t.type;
 		}
 
-		~Ticket(){}
+		~Attribute(){}
 
-		Ticket &operator=(const Ticket &t){
+		Attribute &operator=(const Attribute &t){
 			if(&t == this)
 				return *this;
 			price = t.price;
-			// number = t.number;
-			type = t.type;
+			number = t.number;
+			// type = t.type;
 			return *this;
 		}
 
-		friend ostream& operator<<(ostream &fout, const Ticket &rhs){ //测试用，重载输出
-			fout << "车票种类:\n" << rhs.type << '\n' << "车票单价:\n" << rhs.price << '\n' /*<< "车票余量:\n" << rhs.number << '\n'*/;
+		friend ostream& operator<<(ostream &fout, const Attribute &rhs){ //测试用，重载输出
+			fout << /*"车票种类:\n" << rhs.type << '\n' << */"车票单价:\n" << rhs.price << '\n' << "车票余量:\n" << rhs.number << '\n';
 			return fout;
 		}
 
-		bool operator< (const Ticket& other) {
-			return type < other.type;
+		class tickets_overbooked : public exception {
+		public:
+			tickets_overbooked() : exception(
+				"tickets_overbooked",
+				"Your tickets have been overbooked!!!") {}
+		};
+
+		friend Attribute modifyNumber(const Attribute& attr, const int& delta) {
+			if ((int) attr.number + delta < 0)
+				throw tickets_overbooked();
+			return Attribute(attr.price, attr.number + delta);
 		}
+
+		friend Attribute mergeAttribute(const Attribute& attr0, const Attribute& attr1) {
+			return Attribute(attr0.price + attr1.price,
+				attr0.number < attr1.number ? attr0.number : attr1.number);
+		}
+
+		// bool operator< (const Ticket& other) {
+		// 	return type < other.type;
+		// }
 	};
+
+}
 
 #endif
--- src/test/Ticket_tests.cpp
+++ src/test/Ticket_tests.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
 #include"Ticket.hpp"
 using namespace std;
+using namespace Ticket;
 
 int main(){
-	Ticket t1("一等座", 1000.5);
-    Ticket t2(t1);
-	Ticket t3;
-	Ticket t4 = t1;
+	Ticket::Attribute t1(1000.5, 12);
+    Ticket::Attribute t2(t1);
+	Ticket::Attribute t3;
+	Ticket::Attribute t4 = t1;
 	cout << "t3:\n" << t3 << endl;
 	t3 = t1;
 	cout <<"t1:\n" << t1 << endl << "t2:\n" << t2 << endl << "t3:\n" << t3 << endl << "t4:\n" << t4 << endl;
+
+	Ticket::Attribute t5(20, 100);
+	std::cout << "t5:\n" << t5 << std::endl;
+	cout << "merge: " << mergeAttribute(t1, t5) << std::endl;
+
+	cout << "modify: " << modifyNumber(t5, -20) << std::endl;
+
+	try {
+		modifyNumber(t5, -120);
+	} catch (const Attribute::tickets_overbooked& e) {
+		std::cout << "throw test ok!" << std::endl;
+	}
 
 	return 0;
 }
