f080e2d15672aacd58ee31577fc5c97a80ca933f
footoredo <footoredo@gmail.com>
2017-04-14 00:47:06 +0800

    Update test_runner

--- src/test_runner.cpp
+++ src/test_runner.cpp
@@ -7,6 +7,7 @@ const std::string BIN = "test_bin/";
 int main(int argc, char **argv) {
     std::string object_name(argv[1]);
 
+    system(("rm -f " + BIN + object_name + "_tests").c_str());
     system(("make -B test_" + object_name).c_str());
     system(("./" + BIN + object_name + "_tests").c_str());
 }
