bc3f50f09b41a3908dd5b29e380c4913d43872f2
footoredo <footoredo@gmail.com>
2017-05-08 22:29:45 +0800

    update doc for User regarding password

--- src/APITester.sh
+++ src/APITester.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#url="http://39.108.7.208:3000"
-url="http://localhost:3000"
+url="http://39.108.7.208:3000"
+#url="http://localhost:3000"
 method=$1
 path=$2
 
 if [ $# = 3 ];
 then
-    curl -sX $method --header "Content-Type: application/json" --data $3 $url$path | python -mjson.tool
+    curl -sX $method -H 'Cache-Control: no-cache' --header "Content-Type: application/json" --data $3 $url$path | python -mjson.tool
 else
-    curl -sX $method $url$path | python -mjson.tool
+    curl -sX $method -H 'Cache-Control: no-cache' $url$path | python -mjson.tool
 fi
--- src/test/API_route_tests.py
+++ src/test/API_route_tests.py
@@ -1,7 +1,9 @@
 import requests
 
-#url = "http://39.108.7.208:3000"
-url = "http://localhost:3000"
+url = "http://39.108.7.208:3000"
+#url = "http://localhost:3000"
+s = requests.Session()
+s.headers.update({"Cache-Control": "no-cache"})
 
 route = {
     "name": "G105",
@@ -39,22 +41,22 @@ route = {
     ]
 }
 
-res = requests.post(url + "/stations", json = {"name": "Changzhou North"})
+res = s.post(url + "/stations", json = {"name": "Changzhou North"})
 import json
 route["informations"][0]["stationId"] = stationId0 = res.json()["id"]
-res = requests.post(url + "/stations", json = {"name": "Shanghai Hong Qiao"})
+res = s.post(url + "/stations", json = {"name": "Shanghai Hong Qiao"})
 route["informations"][1]["stationId"] = stationId1 = res.json()["id"]
 
 
-res = requests.post(url + "/routes", json = route);
+res = s.post(url + "/routes", json = route);
 routeId0 = res.json()["id"]
 
-res = requests.get(url + "/routes/" + routeId0 + "/start");
+res = s.get(url + "/routes/" + routeId0 + "/start");
 #print json.dumps(res.json(), indent=4)
 assert res.json()["type"] == "success", "Failed to start the route!"
 print "Start route check passed!"
 
-res = requests.post(url + "/stations/" + stationId0 + "/routes", json = {
+res = s.post(url + "/stations/" + stationId0 + "/routes", json = {
     "stationId": stationId1,
     "date": "2017/5/5"
     })
@@ -62,10 +64,10 @@ result = res.json()["data"]["routes"]
 assert len(result) == 1 and result[0] == routeId0, "Query check failed!"
 print "1st route test passed!"
 
-res = requests.post(url + "/routes", json = route);
+res = s.post(url + "/routes", json = route);
 routeId1 = res.json()["id"]
-res = requests.get(url + "/routes/" + routeId1 + "/start");
-res = requests.post(url + "/stations/" + stationId0 + "/routes", json = {
+res = s.get(url + "/routes/" + routeId1 + "/start");
+res = s.post(url + "/stations/" + stationId0 + "/routes", json = {
     "stationId": stationId1,
     "date": "2017/5/5"
     })
@@ -73,28 +75,28 @@ result = res.json()["data"]["routes"]
 assert len(result) == 2 and routeId0 in result and routeId1 in result, "Query check after 2nd route failed!"
 print "2nd route test passed!"
 
-res = requests.get(url + "/routes/" + routeId0 + "/stop");
+res = s.get(url + "/routes/" + routeId0 + "/stop");
 assert res.json()["type"] == "success", "Failed to stop the 1st route!"
-res = requests.post(url + "/stations/" + stationId0 + "/routes", json = {
+res = s.post(url + "/stations/" + stationId0 + "/routes", json = {
     "stationId": stationId1,
     "date": "2017/5/5"
     })
 result = res.json()["data"]["routes"]
 assert len(result) == 1 and result[0] == routeId1, "Query check after stop 1st route failed!"
 
-res = requests.get(url + "/routes/" + routeId0 + "/stop");
+res = s.get(url + "/routes/" + routeId0 + "/stop");
 #print json.dumps(res.json(), indent=4)
 assert res.json()["type"] == "error", "Duplicated stop check failed!"
 print "stop test passed!"
 
-res = requests.post(url + "/routes/" + routeId1 + "/tickets", json = {
+res = s.post(url + "/routes/" + routeId1 + "/tickets", json = {
     "startStation": stationId0,
     "endStation": stationId1
     })
 assert res.json()["data"]["tickets"]["first class"]["data"]["number"] == 17, "Query tickets check failed!"
 print "Query tickets check passed!"
 
-res = requests.post(url + "/users", json = {"username": "footoredo", "salt": "iamasalt", "password": "."})
+res = s.post(url + "/users", json = {"username": "footoredo", "salt": "iamasalt", "password": "."})
 userId = res.json()["id"]
 
 data = {
@@ -105,7 +107,7 @@ data = {
     "ticketNumber": 1000
     }
 
-res = requests.put(url + "/routes/" + routeId1 + "/tickets", json=data)
+res = s.put(url + "/routes/" + routeId1 + "/tickets", json=data)
 assert res.json()["type"] == "error"
 print "Overbook ticket test passed!"
 data = {
@@ -116,7 +118,7 @@ data = {
     "ticketNumber": 2
     }
 
-res = requests.put(url + "/routes/" + routeId1 + "/tickets", json = {
+res = s.put(url + "/routes/" + routeId1 + "/tickets", json = {
     "userId": userId,
     "startStationId": stationId0,
     "endStationId": stationId1,
@@ -128,17 +130,17 @@ orderJson = res.json()
 orderId = res.json()["id"]
 print "book ticket test passed!"
 
-res = requests.get(url + "/users/" + userId + "/orders");
+res = s.get(url + "/users/" + userId + "/orders");
 orders = res.json()["data"]["orders"]
 assert len(orders) == 1 and orders[0] == orderId, "Order attaching check failed!"
 
-res = requests.get(url + "/users/" + userId + "/orders/" + orderId)
+res = s.get(url + "/users/" + userId + "/orders/" + orderId)
 #print json.dumps(orderJson, indent=4)
 assert res.json() == orderJson, "Order attaching check failed!"
 
 print "Order attaching check passed!"
 
-res = requests.post(url + "/routes/" + routeId1 + "/tickets", json = {
+res = s.post(url + "/routes/" + routeId1 + "/tickets", json = {
     "startStation": stationId0,
     "endStation": stationId1
     })
